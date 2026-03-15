0c24e7a117fde249398128d3d4da2d797f2b7273
sewbej <107070366+sewbej@users.noreply.github.com>
2026-03-15 19:05:17 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'skaner-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './html5-qrcode.min.js',
+  './qrcode.min.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
